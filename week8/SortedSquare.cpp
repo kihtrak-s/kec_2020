d143305c9c4ab31026d341f78a00bf0366549489
kihtrak-s <kihtrak-s@users.noreply.github.com>
2020-08-29 13:06:09 +0530

    Week problem

---
+++ week8/SortedSquare.cpp
@@ -0,0 +1,26 @@
+//Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+// Input: [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+
+// Input: [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+
+int square(int a)
+
+int main()
+{
+    int n=5;
+    int array[] = {-4,-1,0,3,10};
+    int result[] = {0};
+
+
+    int left = 0, right = n-1;
+
+    for (int i=n-1; i>=0; --i)
+    {
+        
+    }
+    
+}
+
